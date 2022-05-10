4ef172cd0de314995090e376480083205610af4b
Williamns Robles <robles.williamns@gmail.com>
2022-05-09 19:44:41 -0500

    creation forms with materialize
    creation forms with materialize

--- hola-mundo/src/App.vue
+++ hola-mundo/src/App.vue
@@ -1,19 +1,95 @@
 <template>
- <div>Hola Mundo</div>
- <button class="btn green" @click="saludar">Boton <i class="material-icons">save</i></button>
+    <div class="container card-panel">
+        <div class="row">
+            <div class="col m4">
+                <label>Nombre</label>
+                <input type="text" v-model="nombre">
+            </div>
+             <div class="col m4">
+                <label>Apellido</label>
+                <input type="text" v-model="apellido">
+            </div>
+        </div>
+    
+        <div class="row">
+            <div class="col m4">
+                <label>Edad</label>
+                <input type="number" v-model="edad">
+            </div>
+            <div class="col m4">
+                <label>Estado Civil</label>
+                <select v-model="estado_civil">
+                    <option value="">Seleccione</option>
+                    <option value="C">Casado</option>
+                    <option value="S">Soltero</option>
+                </select>
+            </div>
+            <div class="col m4">
+                <label>Correo</label>
+                <input type="email" v-model="correo">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col m4 card-panel">
+                <label>Pasatiempos</label>
+                <input type="text" v-model="pasatiempo">
+                <button type="button" class="btn indigo darken-3">Agregar Pasatiempo<i class="material-icons right">send</i></button>
+                <br>
+                <ul>
+                    <li v-for="pasatiempo in pasatiempos" v-bind:key="pasatiempo">{{pasatiempo.descripcion}}</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col m4">
+                <label><input type="checkbox" v-model="suscribirse"><span> Suscribirse al Boletin de Noticias</span></label>
+                <input type="text" v-model="pasatiempo">
+            </div>
+        </div>
+
+         <div class="row">
+            <div class="col">
+                <button type="submit" class="btn indigo darken-4">Agregar Usuario<i class="material-icons right">add_circle</i></button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import M from 'materialize-css'
 export default {
-  name: 'App', 
-  mounted(){
-    this.axios.get('https://prueba.com');
+  name: 'App',
+  components: {
+    
   },
-  methods: {
-      saludar(){
-          M.toast({html: "Hola Mundo"});
-      }
+  data(){
+    return {
+      nombre: '',
+      apellido: '',
+      edad: 0,
+      estado_civil: '',
+      correo: '',
+      pasatiempo: '',
+      pasatiempos : [],
+      select_instances : []
+    }
+  },
+  mounted(){
+      var elems = document.querySelectorAll('select');
+      this.select_instances = M.FormSelect.init(elems, null);
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- hola-mundo/src/main.js
+++ hola-mundo/src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'materialize-css/dist/css/materialize.min.css'
 import 'material-design-icons/iconfont/material-icons.css'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+//import axios from 'axios'
+//import VueAxios from 'vue-axios'
 
-createApp(App).use(VueAxios, axios).mount('#app')
+//createApp(App).use(VueAxios, axios).mount('#app')
+createApp(App).mount('#app')
